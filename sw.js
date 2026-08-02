bb223acc0c22b09546109ecf2b36e2d2c8f7611f
s4work <seiyooon4work@gmail.com>
2026-08-03 01:14:19 +0900

    Turn completed work into timed entries (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "daylist-static-v3";
+const CACHE_NAME = "daylist-static-v4";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
