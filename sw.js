3864bd9858e50862ef6726eeee9ba1238a165b77
s4work <seiyooon4work@gmail.com>
2026-08-03 01:22:33 +0900

    Add iPhone widget view (sw.js)

--- sw.js
+++ sw.js
@@ -1,5 +1,16 @@
-const CACHE_NAME = "daylist-static-v4";
-const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
+const CACHE_NAME = "daylist-static-v5";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./widget.html",
+  "./widget.css",
+  "./widget.js",
+  "./widget.webmanifest",
+  "./icon.svg",
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
